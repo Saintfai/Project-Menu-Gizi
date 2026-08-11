3cb6ad6be13f274ebaf4b80e14c8627431208f49
pairus-portofolio <shevafairuz10@gmail.com>
2026-08-11 09:39:08 +0700

    Add Prisma schema & initial frontend routing
    Update Prisma config and add full schema (User, MenuItem, Order, OrderItem) plus enums (MenuType, OrderStatus, MealTime); set datasource to use DATABASE_URL and enable prisma-client-js driverAdapters. Replace demo App with React Router routes and hierarchy for Patient and Admin. Add PatientLayout and AdminLayout and page stubs: Login, Onboarding, MenuPortal, Cart, Admin Dashboard, MenuCycle. Wrap App with BrowserRouter in main.jsx. UI implementations are TODOs in new files.

---
+++ src/layouts/AdminLayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+
+export default function AdminLayout() {
+  return (
+    <div>
+      {/* TODO: Admin Layout UI (Sidebar & Header) */}
+      <Outlet />
+    </div>
+  );
+}
---
+++ src/pages/Patient/Login.jsx
@@ -0,0 +1,8 @@
+export default function PatientLogin() {
+  return (
+    <div>
+      {/* TODO: Patient Login / RM Verifikasi UI */}
+      Patient Login
+    </div>
+  );
+}
